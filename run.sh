6647f88bf2d1debdb161a291d6ba4d1e38f65bc9
Marty Penner <martypenner@users.noreply.github.com>
2024-01-24 17:46:54 -0500

    fix: send the funnel command to the bg to allow other services to start
    This avoids some proxy errors it seems.

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
 tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=radio
 
-tailscale funnel --bg ${PORT}
+tailscale funnel --bg ${PORT} &
 
 envsubst <config.xml.template >config.xml
 
